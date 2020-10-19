20b8d82612e5ad224bc2075aa6cf550cc52f1c98
Kohaku-Blueleaf <KohakuBlueleaf@users.noreply.github.com>
2020-10-20 00:03:26 +0800

    First commit

--- 指對數表(Example).py
+++ 指對數表(Example).py
@@ -22,20 +22,3 @@ for i in range(1,1001):
 DB.save('./指對數表')
 
 print(DB)
-
-'''
-T1 = time()
-DB.create_index('num')
-DB.create_index('pow2')
-DB.create_index('sqrt')
-DB.create_index('log')
-DB.create_index('ln')
-
-T2 = time()
-DB.save('./指對數表')
-
-T3 = time()
-
-print('Calculate   : {}'.format(str(T1-T0)[:5]))
-print('Create Index: {}'.format(str(T2-T1)[:5]))
-print('Save        : {}'.format(str(T3-T2)[:5]))'''
